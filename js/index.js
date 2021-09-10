733d1976e249239b7eecafadf7087bd4f3fe84d8
Daniel Sobrinho <75446275+daniel-sobrinho@users.noreply.github.com>
2021-09-10 13:11:39 -0300

    blur images effects

--- js/index.js
+++ js/index.js
@@ -2,3 +2,8 @@ $('.carousel').carousel({
     interval: 2800
 })
 
+$(document).ready(()=>{
+    const message = "Atenção, as imagens utilizadas neste projeto foram desfocadas em preservação a imagem das pessoas. Todos os eventos que serão mostrados não foram realizados pelos desenvolvedores, nós apenas utilizamos os eventos e imagens para finalizar a apresentação do trabalho acadêmico."
+    alert(message)
+})
+
